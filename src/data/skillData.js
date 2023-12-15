0e645e8d073cf5dc846b9fbdf638b3ca17b4c08f
clement <ndiclementkinyui@gmail.com>
2023-12-14 21:34:51 -0600

    worked on images route

--- src/data/skillData.js
+++ src/data/skillData.js
@@ -1,86 +1,69 @@
-import express from '../images/ExpressJS.png'
-import git from '../images/git.png'
-import github from '../images/github.png'
-import java from '../images/java.png'
-import javascript from '../images/javascript.png'
-import node from '../images/nodejs.png'
-import python from '../images/python.png'
-import react from '../images/reactjs.png'
-import reduxjs from '../images/redux.png'
-import selenium from '../images/selenium.png'
-import styledComponent from '../images/styled-component.png'
-import tailwind from '../images/tailwind.png'
-import mongodb from '../images/mongodb.jpg'
-import mongoose from '../images/mongoosejs.png'
-import html from '../images/html5.png'
-import css from '../images/css3.png'
-
 
 
 
 export const skills = [
     {
         name:'HTML 5',
-        image:html
+        image:'../images/html5.png'
     },
     {
         name:'CSS 3',
-        image:css
+        image:'../images/css3.png'
     },
     {
         name:'javaScript',
-        image:javascript
+        image:'/Javascript.png'
     },
     {
         name:'react JS',
-        image:react
+        image:'../images/reactjs.png'
     },
     {
         name:'redux',
-        image:reduxjs
+        image:'../images/redux.png'
     },
     {
         name:'node JS',
-        image:node
+        image:'../images/nodejs.png'
     },
     {
         name:'express js',
-        image:express
+        image:'../images/ExpressJS.png'
     },
     {
         name:'mongo DB',
-        image:mongodb
+        image:'../images/mongodb.jpg'
     },
     {
         name:'Mongoose ODM',
-        image:mongoose
+        image:'../images/mongoosejs.png'
     },
     {
         name:'git',
-        image:git
+        image:'../images/git.png'
     },
     {
         name:'gitHub',
-        image:github
+        image:'../images/github.png'
     },
     {
         name:'java',
-        image:java
+        image:'/Java.png'
     },
     {
         name:'python',
-        image:python
+        image:'../images/Python.png'
     },
     {
         name:'selenium',
-        image:selenium
+        image:'../images/selenium.png'
     },
     {
         name:'styled component',
-        image:styledComponent
+        image:'../images/styled-component.png'
     },
     {
         name:'tailwind CSS',
-        image:tailwind
+        image:'../images/tailwind.png'
     },
 ]
