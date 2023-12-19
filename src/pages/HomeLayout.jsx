5bdb498bcd15306c2c978833743e711cf0bd095c
clement <ndiclementkinyui@gmail.com>
2023-12-19 12:07:49 -0600

    worked on the about page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
-import { AnimatePresence } from "framer-motion"
 const HomeLayout = () => {
   return (
     <div className="bg-blue-50 font-thin">
